0c956b52a48f24d9f9f2c938f25f24912479a89a
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-03-12 15:25:26 +0700

    Задача 13. Семина №2 от 11.03. Решена, но есть вопрос

---
+++ Семинар 11.03 ДЗ/Задача 13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if( a > 99 || a < 0) 
+{
+    string str = a.ToString(); 
+    Console.WriteLine(str[2]);
+}
+else
+Console.WriteLine("Третьей цифры нет");
