f9f53f6c8835d4c69e547f7d15b0a655f8d9ec96
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-03-18 19:27:00 +0700

    Task 21. Solution. Dot in 3D

---
+++ ДЗ/Семинар 3 (12.03) ДЗ/Задача 21/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+Console.Write("Введите кординату x1= ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите кординату y1= ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите кординату с1= ");
+double c1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите кординату x2= ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите кординату y2= ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите кординату c2= ");
+double c2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(c1 - c2, 2)));
+
