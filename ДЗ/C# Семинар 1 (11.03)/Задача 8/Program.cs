9065dfd5bce8eb1b27e7864598ecadc7f311625b
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-03-18 17:47:22 +0700

    Updated folder hierarchy

---
+++ ДЗ/C# Семинар 1 (11.03)/Задача 8/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count < Number)
+{
+if(count % 2 == 0)
+    {
+    Console.Write(count + " ");
+    }
+count++;
+}
---
+++ Лекции/Лекция 3 C# 12.03/Program.cs
@@ -0,0 +1,47 @@
+﻿// if(x > 0 && x < 5)
+// {
+//     if (x  == 1 )
+//     Console.WriteLine("X - от нуля до бесконечности. Y - от нуля до бесконечности");
+//     else if (x == 2)
+//      Console.WriteLine("X - от нуля до минус бесконечности. Y - от нуля до бесконечности");
+//     else if (x == 3)
+//         Console.WriteLine("X - от нуля до минус бесконечности. Y - от нуля до  минус бесконечности");
+//     else
+//      Console.WriteLine("X - от нуля до бесконечности. Y - от нуля до  минус бесконечности");
+// }
+// else
+// Console.WriteLine("Не верная четверть!");
+
+
+
+// 1. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+
+
+// Console.Clear();
+// Console.Write("Введите кординаты точки A по оси X=  ");
+// double A1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите кординаты точки A по оси Y=  ");
+// double A2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите кординаты точки B по оси X=  ");
+// double B1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите кординаты точки B по оси Y=  ");
+// double B2 = Convert.ToDouble(Console.ReadLine());
+
+// double A = A1 - B1;
+// double B = A2 - B2;
+// A = Math.Pow(A, 2);
+// B = Math.Pow(B, 2);
+// double distance = Math.Sqrt(A + B);
+// Console.WriteLine(distance);
+
+
+// Console.Clear();
+// Console.Write("n =  ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i <= n; i++) 
+//     Console.Write($"{Math.Pow(i, 2)} ");
