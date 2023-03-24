66ecda3610572a94bac888719a3187f1624f20a4
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-03-25 00:48:30 +0700

    Task 29: Write a program that will create an array of 8 characters and fill in values ​​from the user

---
+++ ДЗ/Семинар 4 (18.03) ДЗ/Задача 29. Заполнение массива пользователем/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая будет создавать массив из 8 символов и заполнять значениями от пользователя
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+void InputArray(int[] array)
+{
+    int number = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите число: ");
+        number = Convert.ToInt32(Console.ReadLine());
+        array[i] = number;      
+    }        
+}
+
+
+Console.Clear();
+Console.Write("Введите колличество символов которые вы хотите внести в массив: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
