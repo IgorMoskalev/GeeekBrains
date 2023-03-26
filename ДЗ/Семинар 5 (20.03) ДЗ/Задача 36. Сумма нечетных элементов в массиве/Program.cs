3c0802de5b1b9cc1af247d717aa35ecaa4c1b5c3
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-03-26 14:30:32 +0700

    Task 36: Define a one-dimensional array filled with random numbers. Find the sum of elements in odd positions.

---
+++ ДЗ/Семинар 5 (20.03) ДЗ/Задача 36. Сумма нечетных элементов в массиве/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+void InputArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+		array[i] = new Random().Next(-10000, 10000); 
+}
+
+void even (int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            summ = summ + array[i];
+        }
+    }
+    Console.Write($"Сумма не чётных чисел в массиве: {summ}");
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+even(array);
