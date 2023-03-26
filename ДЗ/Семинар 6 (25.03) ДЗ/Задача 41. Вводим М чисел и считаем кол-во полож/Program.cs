813bbb1227f3e035bd6f0f6e3d22864d6eb49d35
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-03-26 15:15:20 +0700

    Task 41. The user enters M numbers from the keyboard. Count how many numbers greater than 0 the user entered

---
+++ ДЗ/Семинар 6 (25.03) ДЗ/Задача 41. Вводим М чисел и считаем кол-во полож/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+void NumberOfPositiveInArray (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.Write($"Колличество положительных чисел = {count}");
+}
+
+Console.Clear();
+Console.Write("Введите числа через пробел: ");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+NumberOfPositiveInArray(array);
