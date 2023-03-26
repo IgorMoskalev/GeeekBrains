1a846d4f65cc45a5c65d99088b8c67213fe71dc0
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-03-26 15:02:30 +0700

    Task 38.Specify an array of real numbers. Find the difference between the maximum and minimum elements of the array

---
+++ ДЗ/Семинар 5 (20.03) ДЗ/Задача 38. Разница между Макс и Мин/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+void InputArray(double[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+		array[i] = Math.Round(new Random().NextDouble() * (100 - 1) + 1); 
+}
+
+void DifferenceBetweenMaxAndMinArray (double[] array)
+{
+    double max = array[0], min = array[0], diff = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+        else if (array[i] < min)
+                min = array[i];
+    }
+    Console.Write($"Разниза между максимальным: {max} и минимальным: {min}, равна: {diff = max - min}");
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[size];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+DifferenceBetweenMaxAndMinArray(array);
