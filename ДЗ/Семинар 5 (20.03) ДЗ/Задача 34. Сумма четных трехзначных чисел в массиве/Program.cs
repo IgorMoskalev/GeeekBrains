478ca4d8027e5a5b8ad918ce7277a944de81f5f4
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-03-26 14:19:36 +0700

    Task 34. Specify an array filled with random positive three-digit numbers. Write a program to show the number of even numbers in an array

---
+++ ДЗ/Семинар 5 (20.03) ДЗ/Задача 34. Сумма четных трехзначных чисел в массиве/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+ void InputArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+		array[i] = new Random().Next(100, 1000); // [-9, 9]
+}
+
+void even (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.Write($"Количество чётных чисел в массиве: {count}");
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+even(array);
