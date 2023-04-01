59cec86e59385d3a32468e3aebf1ad66d75bc574
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-04-01 15:12:31 +0700

    Task 47.Specify a two-dimensional m×n array filled with random real numbers

---
+++ ДЗ/Семинар 7 (26.03) ДЗ/Задача 47. Двумерный массив. Случайные числа/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (10 + 2) - 2, 2);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+double[, ] matrix = new double [size [0], size [1]];
+InputMatrix (matrix);
