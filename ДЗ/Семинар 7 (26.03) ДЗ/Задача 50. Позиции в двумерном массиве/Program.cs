43415dd162e8b7b3f88fa1ac3861b0021fcd1899
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-04-01 15:34:22 +0700

    Task 50.Write a program that takes the position of an element in a two-dimensional array as input and returns the value of that element or an indication that there is no such element.

---
+++ ДЗ/Семинар 7 (26.03) ДЗ/Задача 50. Позиции в двумерном массиве/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+
+void InputMatrix(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 50);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void PrintPozition(int [, ] matrix)
+{
+    Console.Write("Введите позицию элемента через запятую: ");
+    int[] pozition = Console.ReadLine().Split(",").Select(x => int.Parse(x)).ToArray();
+    if (matrix.GetLength(0) >= pozition[0]
+        && matrix.GetLength(1) >= pozition[1])
+    {
+        Console.Write($"Под введенной позицией находится число: {matrix[pozition[0], pozition[1]]}");
+
+    }
+    else Console.Write("Такой позиции нет");
+}
+
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[, ] matrix = new int [size [0], size [1]];
+InputMatrix (matrix);
+PrintPozition (matrix);
