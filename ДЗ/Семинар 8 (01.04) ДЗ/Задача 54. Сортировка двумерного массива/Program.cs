60e869456ea37c235bc4a25edce526cf0ce8ac15
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-04-02 13:00:26 +0700

    Task 54.Specify a two-dimensional array. Write a program to sort the elements of each row of a two-dimensional array in descending order.

---
+++ ДЗ/Семинар 8 (01.04) ДЗ/Задача 54. Сортировка двумерного массива/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+ int[, ] InputMatrix()
+{
+    Console.Write("Введите размер матрицы: ");
+    int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+    int[, ] matrix = new int[size[0], size[1]];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 50);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortArrayByString(int[, ] matrix)
+{
+    int[] array = new int[matrix.GetLength(0)];
+      for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[j] = matrix[i, j];
+        }
+        Array.Sort(array);
+        Array.Reverse(array);
+        for (int j = 0; j < array.Length; j++)
+        {
+            matrix[i, j] = array[j];
+        }
+    }
+}
+
+
+Console.Clear();
+int[, ] matrix = InputMatrix();
+SortArrayByString(matrix);
+PrintMatrix(matrix);
