51ee7a2c1ba4b55691bc159f8c6158141891d9c8
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-04-02 17:08:43 +0700

    Task 56. Specify a rectangular two-dimensional array. Write a program to find the string with the smallest sum of elements

---
+++ ДЗ/Семинар 8 (01.04) ДЗ/Задача 56. Строка с min суммой/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[, ] InputMatrix()
+{
+    Console.Write("Введите размер матрицы: ");
+    int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+    int[, ] matrix = new int[size[0], size[1]];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 5);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return matrix;
+}
+
+
+void MinSumStringInArray(int[, ] matrix)
+{
+    int min = 0;
+    int minString = 1;
+    int firstString = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            firstString = firstString + matrix[0, j];
+            min = firstString;
+        }
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        int sumString = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumString = sumString + matrix[i, j];
+            
+        }
+        if (sumString < min)
+        {
+            min = sumString;
+            minString = i + 1;
+        }
+        
+    }
+    Console.WriteLine($"Строка {minString} это строка c минимальной суммой элементов");
+
+}
+
+
+Console.Clear();
+int[, ] matrix = InputMatrix();
+MinSumStringInArray(matrix);
