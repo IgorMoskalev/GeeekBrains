b2c662c1825ecb105f3ccc22b7fd35face7ed496
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-04-08 15:32:26 +0700

    Task 64. Set the value to N. Write a program that prints all natural numbers between N and 1. Execute using recursion.

---
+++ ДЗ/Семинар 9 (02.04) ДЗ/Задаа 64. Натуральные числа до N рекурсия/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+string OutPutNumber(int Number)
+{
+    if (Number == 1)
+        return 1.ToString();
+    return Number.ToString() + ", " + OutPutNumber(Number - 1);
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write(OutPutNumber(number));
