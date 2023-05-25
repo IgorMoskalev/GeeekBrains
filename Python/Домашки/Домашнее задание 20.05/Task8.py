dbcc85665de7ad64080f06eb9097797d8ae075a9
IgorMoskalev <moskalevmodulbank@gmail.com>
2023-05-25 20:46:12 +0700

    Семинар

--- Python/Домашки/Домашнее задание 20.05/Task8.py
+++ Python/Домашки/Домашнее задание 20.05/Task8.py
@@ -3,4 +3,4 @@ m = int(input("Введите M: "))
 k = int(input("Введите K: "))
 if k % n == 0 or k % m == 0:
     print("Yes")
-else: print("No")
+else: print("No")   
---
+++ Python/Семинар/Задача 11.py
@@ -0,0 +1,23 @@
+# a = int (input("a= "))
+# n = int (input("n= "))
+
+# first = 0
+# second = 1
+# fibonachi = 0
+# count = 0
+
+# for i in range (n):
+#     if fibonachi != a:
+#         fibonachi = first + second
+
+#         count = count + 1
+#     else: print(count)
+
+
+n = int(input("Введите число дней от 1 до 100: "))
+if n > 0 and n < 101
+    a = 0
+    for i in range (n):
+        a = int(input("Введите температуру: "))
+        if a < 0:
+                
